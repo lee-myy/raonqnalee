ceddb69eceb90e9269996bc78c556e7656b90b95
leemy <andud101@naver.com>
2019-07-28 06:52:09 +0900

    initial commit

---
+++ src/main/java/com/raon/raonqna/leemooyoung/RaonqnaApplication.java
@@ -0,0 +1,13 @@
+package com.raon.raonqna.leemooyoung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaonqnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaonqnaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/raon/raonqna/leemooyoung/raoncontroller.java
@@ -0,0 +1,5 @@
+package com.raon.raonqna.leemooyoung;
+
+public class raoncontroller {
+
+}
